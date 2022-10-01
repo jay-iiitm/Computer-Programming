0e02d80a0c2938404fd14e309603377c5a6452e9
Jay Raikhere <56078790+jayraikhere@users.noreply.github.com>
2022-10-01 11:34:17 +0530

    added priority queue for first come first serve

---
+++ priority.cpp
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int bt[100],p[100],wt[100],tat[100],pr[100],i,j,n,pos,temp;
+    float avgwt,avgtat,total=0;
+    printf("Enter Total Number of Process:");
+    scanf("%d",&n);
+    printf("\nEnter Burst Time and Priority\n");
+    for(i=0;i<n;i++)
+    {
+        printf("\nP%d\n",i+1);
+        printf("Burst Time:");
+        scanf("%d",&bt[i]);
+        printf("Priority:");
+        scanf("%d",&pr[i]);
+        p[i]=i+1;           
+    }
+    for(i=0;i<n;i++)
+    {
+        pos=i;
+        for(j=i+1;j<n;j++)
+        {
+            if(pr[j]<pr[pos])
+                pos=j;
+        }
+        temp=pr[i];
+        pr[i]=pr[pos];
+        pr[pos]=temp;
+ 
+        temp=bt[i];
+        bt[i]=bt[pos];
+        bt[pos]=temp;
+ 
+        temp=p[i];
+        p[i]=p[pos];
+        p[pos]=temp;
+    }
+    wt[0]=0;	
+    for(i=1;i<n;i++)
+    {
+        wt[i]=0;
+        for(j=0;j<i;j++)
+            wt[i]+=bt[j];
+ 
+        total+=wt[i];
+    }
+ 
+    avgwt=total/n;      
+    total=0;
+ 
+    printf("\nProcess\t    Burst Time    \tWaiting Time\tTurnaround Time");
+    for(i=0;i<n;i++)
+    {
+        tat[i]=bt[i]+wt[i];    
+        total+=tat[i];
+        printf("\nP%d\t\t  %d\t\t    %d\t\t\t%d",p[i],bt[i],wt[i],tat[i]);
+    }
+    avgtat=total/n;     
+    printf("\n\nAverage Waiting Time=%f",avgwt);
+    printf("\nAverage Turnaround Time=%f\n",avgtat);
+    getch();
+    return 0;
+}
