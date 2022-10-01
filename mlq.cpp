ea94ef13aa2005ae4330d42d21c97a55a9f180a2
Niketan0109 <56088377+Niketan0109@users.noreply.github.com>
2022-10-01 12:53:15 +0530

    Update mlq.cpp

--- mlq.cpp
+++ mlq.cpp
@@ -88,3 +88,23 @@ public:
     return head;
     }
 };
+
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+       int l=0;
+       int r = arr.size()-1;
+       while(r-l>=k)
+       {
+          if(x-arr[l]<=arr[r]-x)
+          {
+              r--;
+          }
+           else{
+               l++;
+           }
+       }
+        return vector<int>(begin(arr)+l,begin(arr)+r+1);
+    }
+};
+
